bd2d5d8a4b7a2ac4547014384648a2cff27a0ed4
Unknown <victoria_roger@hotmail.fr>
2018-02-09 16:07:41 +0100

    Premier push
    Si j'ai de la chance ça casse rien

--- src/Solveur/ParseurArff.java
+++ src/Solveur/ParseurArff.java
@@ -1,6 +1,12 @@
 package Solveur;
 
 import java.util.ArrayList;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader.ArffReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ParseurArff {
 	
@@ -15,8 +21,39 @@ public class ParseurArff {
 		 * 
 		 * car on attend un tableau de string qui correspond au couple (field, value)
 		 */
+
+	
+
+	/**Affiche le contenu du fichier. Cette fonction va disparaître prochainement, mais ça fait une base.
+	*/
+	ArffReader arff;
+	try {
+		BufferedReader reader =	new BufferedReader(new FileReader("./docs/weather.nominal.arff"));
+		arff = new ArffReader(reader);
+		Instances data = arff.getData();
+		data.setClassIndex(data.numAttributes()-1);
+		
+		for (int i = 0; i <= data.numInstances() - 1; i++) {
+		    Instance instance = data.get(i);
+		    list.add([instance.attribute(i%5).name(),instance.stringValue(i)]);	
+
+		}
+		
+	} catch (FileNotFoundException e){
+		e.printStackTrace();
+	}
+	
+	catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
 		
 		return list;
+	}
+	/**Fonction présente pour le test*/
+	public static void main(String[] args) {
+		ParseurArff ra = new ParseurArff();
 	}
 
 }
