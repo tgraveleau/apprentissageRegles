bbf2f758b606e4358d700d568263abc48461ac0d
Unknown <victoria_roger@hotmail.fr>
2018-02-12 12:30:22 +0100

    Modif Parse
    La liste des valeurs est ajoutée dans la table

--- src/Solveur/ParseurArff.java
+++ src/Solveur/ParseurArff.java
@@ -27,15 +27,21 @@ public class ParseurArff {
 	/**Affiche le contenu du fichier. Cette fonction va disparaître prochainement, mais ça fait une base.
 	*/
 	ArffReader arff;
+	String[] vals = new String[5];
 	try {
-		BufferedReader reader =	new BufferedReader(new FileReader("./docs/weather.nominal.arff"));
+		BufferedReader reader =	new BufferedReader(new FileReader(filename));
 		arff = new ArffReader(reader);
 		Instances data = arff.getData();
 		data.setClassIndex(data.numAttributes()-1);
 		
-		for (int i = 0; i <= data.numInstances() - 1; i++) {
+		for (int i = 0; i <= data.numInstances()- 1; i++) {
 		    Instance instance = data.get(i);
-		    list.add([instance.attribute(i%5).name(),instance.stringValue(i)]);	
+		    vals[0]=instance.stringValue(0);
+		    vals[1]=instance.stringValue(1);
+		    vals[2]=instance.stringValue(2);
+		    vals[3]=instance.stringValue(3);
+		    vals[4]=instance.stringValue(4);
+		    list.add(vals);	
 
 		}
 		
@@ -54,6 +60,8 @@ public class ParseurArff {
 	/**Fonction présente pour le test*/
 	public static void main(String[] args) {
 		ParseurArff ra = new ParseurArff();
+		ArrayList<String[]> vals = ra.parse("./docs/weather.nominal.arff");
+		System.out.println(vals);
 	}
 
 }
